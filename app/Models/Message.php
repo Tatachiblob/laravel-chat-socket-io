34d675a5e9551537e5e90b0024a2f6d54f371a1d
Yuta Inoue <58018461+YutaInoueCommudePH@users.noreply.github.com>
2020-11-03 19:56:09 +0900

    Create migrations and models
    [add] Migration files
    [add] Model files
    
    Co-Authored-By: Tatachiblob <20595225+Tatachiblob@users.noreply.github.com>

---
+++ app/Models/Message.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'group_id', 'user_id', 'message'
+    ];
+
+    /**
+     * Get the user that owns the message.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the group that owns the message.
+     */
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+}
