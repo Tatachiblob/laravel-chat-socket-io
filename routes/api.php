b131407531591cb1f226ffde50d82d3fe18d05c1
Yuta Inoue <58018461+YutaInoueCommudePH@users.noreply.github.com>
2020-11-03 18:48:58 +0900

    Setup socket io
    [add] PublicEvent for sending sample broadcast
    
    Co-Authored-By: Tatachiblob <20595225+Tatachiblob@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('public-event', function () {
+    event(new \App\Events\PublicEvent);
+    return 'public';
+});
